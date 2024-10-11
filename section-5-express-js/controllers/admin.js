033d7cc06f3edbc61757acd1e72ad4ae728a66d2
Tia Widi <tia.septianawidi@gmail.com>
2024-10-11 21:46:07 +0700

    Section 8 - Registering Routes

--- section-5-express-js/controllers/admin.js
+++ section-5-express-js/controllers/admin.js
@@ -14,18 +14,14 @@ exports.postAddProduct = (req, res, next) => {
   const product = new Product(req.body.title);
   product.save();
   res.redirect('/');
-}
+};
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll((products) => {
-    res.render('shop/product-list', {
+  Product.fetchAll((products) => {
+    res.render('admin/products', {
       prods: products, 
-      pageTitle: 'Shop',
-      path: '/',
-      hasProducts: products.length > 0,
-      activeShope: true,
-      productCSS: true
+      pageTitle: 'Admin Products',
+      path: '/admin/products'
     });
-
   });
-}
+};
