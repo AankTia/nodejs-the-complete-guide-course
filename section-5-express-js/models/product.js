59b143570db52e2d134ad9d169004677ca44d343
Tia Widi <tia.septianawidi@gmail.com>
2024-10-13 21:56:50 +0700

    Section 9 - Dynamic Routes & Advance Models

--- section-5-express-js/models/product.js
+++ section-5-express-js/models/product.js
@@ -45,6 +45,17 @@ module.exports = class Product {
     });
   }
 
+  static deleteById(id) {
+    getProductFromFile(products => {
+      const updatedProducts = products.filter(prod => prod.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+        if(!err) {
+          
+        }
+      });
+    });
+  }
+
   static fetchAll(cb) { //cb: callback
     getProductFromFile(cb);
   }
