e66ee1db3a5990fb8e36a2156c0bbadbe0972acb
Tia Widi <tia.septianawidi@gmail.com>
2024-10-16 20:03:28 +0700

    Section 11 - Defining a Product Model

--- section-5-express-js/models/product.js
+++ section-5-express-js/models/product.js
@@ -1,48 +1,27 @@
-// const fs = require('fs');
-// const path = require('path');
-const db = require('../util/database');
+const Sequelize = require('sequelize');
 
-const Cart = require('./cart');
+const sequilize = require('../util/database');
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+const Product = sequilize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequilize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
   }
+});
 
-  save() {
-    return db.execute(
-      'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
-      [this.title, this.price, this.imageUrl, this.description]
-    );
-  }
-
-  static deleteById(id) {
-    // getProductFromFile(products => {
-    //   const product = products.find(prod => prod.id === id);
-    //   const updatedProducts = products.filter(prod => prod.id !== id);
-    //   fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-    //     if(!err) {
-    //       Cart.deleteProduct(id, product.price);
-    //     }
-    //   });
-    // });
-  }
-
-  // // Fetch Data from File
-  // static fetchAll(cb) { //cb: callback
-  //   getProductFromFile(cb);
-  // }
-
-  // Fetch Data from MySQL
-  static fetchAll() {
-    return db.execute('SELECT * FROM products');
-  }
-
-  static findById(id, ){
-    return db.execute('SELECT * FROM products where products.id = ?', [id]);
-  };
-}
+module.exports = Product;
