5e0f3d98f08d9a99533141075602b40e60506850
Tia Widi <tia.septianawidi@gmail.com>
2024-10-16 19:56:03 +0700

    Section 11 - Connecting to Database with sequelize

---
+++ section-5-express-js/section-11-notes.md
@@ -0,0 +1,14 @@
+# Understanding Sequilize
+## What is Sequilize?
+An Object Relation Mapping Library
+
+## Core Concepts
+- Models --> e.q. User, Product
+- Intances --> const user = User.build()
+- Queries --> User.findAll()
+- Associations --> User.hasMany(Product)
+
+## Install Sequilize
+npm install --save sequelize
+
+Sequilize need mysql2
--- section-5-express-js/util/database.js
+++ section-5-express-js/util/database.js
@@ -1,10 +1,18 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'node-course',
-  password: 'tw877121'
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   database: 'node-course',
+//   password: 'tw877121'
+// });
+
+// module.exports = pool.promise();
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('node-course', 'root','tw877121', {
+  dialect: 'mysql', 
+  host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
