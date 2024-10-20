6465a6c7424f4c1d144897a36b398e3fba4897bf
Tia Widi <tia.septianawidi@gmail.com>
2024-10-20 19:31:45 +0700

    Section 12: Fetchig all products

--- section-5-express-js/controllers/shop.js
+++ section-5-express-js/controllers/shop.js
@@ -3,8 +3,7 @@ const Product = require('../models/product');
 // const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
-  Product
-    .findAll()
+  Product.fetchAll
     .then(products => {
       res.render('shop/product-list', {
         prods: products,
@@ -45,7 +44,7 @@ exports.getProduct = (req, res, next) => {
 
 exports.getIndex = (req, res, next) => {
   Product
-    .findAll()
+    .fetchAll()
     .then(products => {
       res.render('shop/index', {
         prods: products,
