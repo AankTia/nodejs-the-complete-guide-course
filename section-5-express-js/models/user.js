3a92b5c1ad8fc7d29395028412ca3149fff1ed71
Tia Widi <tia.septianawidi@gmail.com>
2024-10-22 19:50:21 +0700

    sect-12: Getting orders

--- section-5-express-js/models/user.js
+++ section-5-express-js/models/user.js
@@ -110,7 +110,9 @@ class User {
 
   getOrders() {
     const db = getDb();
-
+    return db.collection("orders")
+      .find({'user._id': new ObjectId(this._id)})
+      .toArray();
   }
 
   static findById(userId) {
