9a3ee3b017bf717ba372dce26bb5e805c024bcca
Tia Widi <tia.septianawidi@gmail.com>
2024-10-23 20:25:47 +0700

    Sec13 : Using Relations in Mongoose

--- section-5-express-js/controllers/admin.js
+++ section-5-express-js/controllers/admin.js
@@ -17,7 +17,8 @@ exports.postAddProduct = (req, res, next) => {
     title: title,
     price: price,
     description: description,
-    imageUrl: imageUrl
+    imageUrl: imageUrl,
+    userId: req.user
   });
   product
     .save()
@@ -82,7 +83,10 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.find()
+    // .select('title price -_id')
+    // .populate('userId', 'name')
     .then(products => {
+      console.log(products);
       res.render('admin/products', {
         prods: products, 
         pageTitle: 'Admin Products',
