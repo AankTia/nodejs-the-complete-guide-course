97bac45875be2d210a78b5219522a2006cdec71c
Tia Widi <tia.septianawidi@gmail.com>
2024-10-25 20:06:06 +0700

    Sec13 - Creating an Order

--- section-5-express-js/controllers/shop.js
+++ section-5-express-js/controllers/shop.js
@@ -1,6 +1,6 @@
 const Product = require('../models/product');
 // const Cart = require('../models/cart');
-// const Order = require('../models/order');
+const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -85,9 +85,22 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart;
   req.user
-    .addOrder()
+    .populate('cart.items.productId')
+    // .execPopulate()
+    .then(user => {
+      const products = user.cart.items.map(i => {
+        return { quantity: i.quantity, product: { ...i.productId._doc } };
+      });
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user
+        },
+        products: products
+      });
+      return order.save();
+    })
     .then(result => {
       res.redirect("/orders");
     })
