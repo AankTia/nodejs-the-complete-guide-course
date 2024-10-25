3f7a0c58d4261f31f0d544b11118a9cdc1f2748e
Tia Widi <tia.septianawidi@gmail.com>
2024-10-26 04:50:40 +0700

    Sec14 - Adding arequest driven logic solution

--- section-5-express-js/controllers/shop.js
+++ section-5-express-js/controllers/shop.js
@@ -6,10 +6,11 @@ exports.getProducts = (req, res, next) => {
   Product.find()
     .then(products => {
       console.log(products);
-      res.render('shop/product-list', {
+      res.render("shop/product-list", {
         prods: products,
-        pageTitle: 'All Products',
-        path: '/products'
+        pageTitle: "All Products",
+        path: "/products",
+        isLoggedIn: req.isAuthenticated,
       });
     })
     .catch(err => {
@@ -21,10 +22,11 @@ exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId)
     .then(product => {
-      res.render('shop/product-detail', {
+      res.render("shop/product-detail", {
         product: product,
         pageTitle: product.title,
-        path: '/products'
+        path: "/products",
+        isLoggedIn: req.isAuthenticated,
       });
     })
     .catch(err => console.log(err));
@@ -33,10 +35,11 @@ exports.getProduct = (req, res, next) => {
 exports.getIndex = (req, res, next) => {
   Product.find()
     .then(products => {
-      res.render('shop/index', {
+      res.render("shop/index", {
         prods: products,
-        pageTitle: 'Shop',
-        path: '/'
+        pageTitle: "Shop",
+        path: "/",
+        isLoggedIn: req.isAuthenticated,
       });
     })
     .catch(err => {
@@ -50,10 +53,11 @@ exports.getCart = (req, res, next) => {
     // .execPopulate()
     .then(user => {
       const products = user.cart.items;
-      res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart',
-        products: products
+      res.render("shop/cart", {
+        path: "/cart",
+        pageTitle: "Your Cart",
+        products: products,
+        isLoggedIn: req.isAuthenticated,
       });
     })
     .catch(err => console.log(err));
@@ -117,6 +121,7 @@ exports.getOrders = (req, res, next) => {
         path: "/orders",
         pageTitle: "Your Orders",
         orders: orders,
+        isLoggedIn: req.isAuthenticated
       });
     })
     .catch((err) => console.log(err));
