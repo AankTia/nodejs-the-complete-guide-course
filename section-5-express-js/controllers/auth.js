ec22b13fc09af781d6625ae14591c96361aeaf93
Tia Widi <tia.septianawidi@gmail.com>
2024-10-26 05:18:52 +0700

    Sec14 - Initializing the Session Middleware

--- section-5-express-js/controllers/auth.js
+++ section-5-express-js/controllers/auth.js
@@ -1,5 +1,7 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.get("Cookie").trim().split("=")[1];
+  const isLoggedIn = req.get("Cookie")
+    .trim()
+    .split("=")[1] == 'true';
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
@@ -8,6 +10,6 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true')
+  res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly')
   res.redirect('/');
 };
--- section-5-express-js/section-14-notes.md
+++ section-5-express-js/section-14-notes.md
@@ -1,3 +1,9 @@
 # Section 14: Session & Cookies
 
 ## What's a Cookie?
+in a client side
+
+## What's a Session?
+in a server side
+
+npm install --save express-session
